2f4dcd9fe82a6debccc6c58d51436a9362085ce8
dina-hagar <dina.hagar@sharwa.io>
2023-09-23 17:55:16 +0200

    home page

--- bosta/src/pages/Home/Home.js
+++ bosta/src/pages/Home/Home.js
@@ -1,8 +1,24 @@
 import React from 'react'
+import { Box, Grid, Typography } from '@mui/material';
+import './Home.css';
+import bostaImg2 from '../../images/bostaImg2.png';
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <Box className="homePage">
+      <Grid container spacing={2}>
+        <Grid item xs={12} sm={12} md={6} sx={{ paddingTop: '30px !important' }} className='leftSide'>
+          <Typography className='homeHeaderTitle'>Join A New Generation Of Logistics!</Typography>
+          <Typography className='homeHeaderParagraph'>
+            Redefining how you ship, track, collect, deliver all through innovative tech-solutions and efficient operations.
+          </Typography>
+          <button className='homeHeaderBtn'>Start Now</button>
+        </Grid>
+        <Grid item xs={12} sm={12} md={6} className='homeImgBox'>
+          <img src={bostaImg2} alt="" className='homeImg'/>
+        </Grid>
+      </Grid>
+    </Box>
   )
 }
 
